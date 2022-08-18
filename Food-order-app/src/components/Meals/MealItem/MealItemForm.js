7484f3c27220f312df7fd8f1cd0c6edc8bf5520c
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 20:47:10 +0000

    update

--- Food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ Food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,12 @@
-import classes from './MealItemForm.module.css';
+import classes from "./MealItemForm.module.css";
 
-const MealItemForm = props => {};
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form} >
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
