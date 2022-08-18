51db9ad4c687c4e3fcc9569b7399ec1a95c323b7
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 16:32:58 +0000

    update in app.js

--- Food-order-app/src/App.js
+++ Food-order-app/src/App.js
@@ -1,8 +1,11 @@
+import { Fragment } from 'react';
+import Header from './components/Layout/Header.js';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
