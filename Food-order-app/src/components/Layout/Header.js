58e4f122e1ecf98fae7d6f90c054cc421dd506e3
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 16:33:06 +0000

    update in header

--- Food-order-app/src/components/Layout/Header.js
+++ Food-order-app/src/components/Layout/Header.js
@@ -1,16 +1,19 @@
-import { Fragment } from 'react';
-import mealsImage from '../../assets/mealsImage.jpg';
-import classes from './Header.module.css';
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header} >
-            <h1>ReactMeals</h1>
-            <div className={classes['main-image']} >
-                <img src={mealsImage} alt='A table full of delicious food!' />
-            </div>
-        </header>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
     </Fragment>
+  );
 };
 
 export default Header;
