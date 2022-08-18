1fd033ca9440841135037f5a06cb17b16f7bf0c8
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 20:10:14 +0000

    update

--- Food-order-app/src/components/Meals/AvailableMeals.js
+++ Food-order-app/src/components/Meals/AvailableMeals.js
@@ -29,7 +29,7 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
 
-      const mealsList = DUMMY_MEALS.map(meal => <li>meal.name</li>)
+      const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
 
     return <section className={classes.meal} >
         <ul>
