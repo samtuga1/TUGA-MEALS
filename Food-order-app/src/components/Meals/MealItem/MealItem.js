9ea6a533537413b0e8936f7310e261ec53c42983
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 20:34:41 +0000

    update

--- Food-order-app/src/components/Meals/MealItem/MealItem.js
+++ Food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,17 @@
 import classes from './MealItem.module.css';
 
-const MealItem = props => {};
+const MealItem = props => {
+    const price = `$${props.price.toFixed(2)}`;
+    return <li>
+        <div>
+            <h3>{props.name}</h3>
+            <div>{props.description}</div>
+            <div>{price}</div>
+        </div>
+        <div>
+
+        </div>
+    </li>
+};
 
 export default MealItem;
