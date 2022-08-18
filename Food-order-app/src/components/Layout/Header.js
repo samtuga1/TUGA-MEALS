ee8fff72bd1cbe328a4a869f56fc3c4232ee20d7
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 16:28:19 +0000

    update

--- Food-order-app/src/components/Layout/Header.js
+++ Food-order-app/src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import mealsImage from '../../assets/mealsImage.jpg';
+import classes from './Header.module.css';
 
 const Header = props => {
     return <Fragment>
-        <header>
+        <header className={classes.header} >
             <h1>ReactMeals</h1>
-            <div>
+            <div className={classes['main-image']} >
                 <img src={mealsImage} alt='A table full of delicious food!' />
             </div>
         </header>
