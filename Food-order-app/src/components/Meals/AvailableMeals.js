bb0db3e6d266fdbae833a70792ebcccdc4954925
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 20:40:17 +0000

    update

--- Food-order-app/src/components/Meals/AvailableMeals.js
+++ Food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,44 +1,51 @@
-import classes from './AvailableMeals.module.css';
-import Card from '../UI/Card';
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-      const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
-
-    return <section className={classes.meals} >
-        <ul>
-          <Card>
-             {mealsList}
-          </Card>
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <ul>
+        <Card>{mealsList}</Card>
+      </ul>
     </section>
+  );
 };
 
 export default AvailableMeals;
