82842020a8e6302aa74d23d923a7664082eb2dd7
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 21:33:58 +0000

    update

--- Food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ Food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -3,15 +3,18 @@ import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
   return (
-    <form className={classes.form} >
-      <Input label='Amount' input={{
-        id : 'amount',
-        type : 'number',
-        min : '1',
-        step : '1',
-        max : '5',
-        defaultValue : '1'
-      }} />
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: props.id,
+          type: "number",
+          min: "1",
+          step: "1",
+          max: "5",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
