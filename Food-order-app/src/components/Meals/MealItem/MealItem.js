2fe1fc9849e84ca1ee4ed40907ff0e6ecb47a726
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 20:52:40 +0000

    update

--- Food-order-app/src/components/Meals/MealItem/MealItem.js
+++ Food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
@@ -9,7 +10,7 @@ const MealItem = props => {
             <div className={classes.price} >{price}</div>
         </div>
         <div>
-
+            <MealItemForm />
         </div>
     </li>
 };
