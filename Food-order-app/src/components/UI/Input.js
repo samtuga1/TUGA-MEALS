154d783fda04511e7fd6fdc0e243f79f7515d623
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 21:03:23 +0000

    update

--- Food-order-app/src/components/UI/Input.js
+++ Food-order-app/src/components/UI/Input.js
@@ -2,8 +2,8 @@ import classes from './Input.module.css';
 
 const Input = props => {
     return <div className={classes.input} >
-        <label htmlFor ></label>
-        <input></input>
+        <label htmlFor={props.input.id} > {props.label} </label>
+        <input {...props.input} />
     </div>
 };
 
