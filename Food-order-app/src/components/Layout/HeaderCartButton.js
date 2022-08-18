3c0bd023db872d077b4a49fd71cda997532111c5
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 19:32:38 +0000

    update

--- Food-order-app/src/components/Layout/HeaderCartButton.js
+++ Food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,16 @@
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = props => {
-    return <button className={classes.button} >
-        <span className={classes.icon} >
-            <CartIcon />
-        </span>
-        <span>
-            Your cart
-        </span>
-        <span className={classes.badge} >
-            3
-        </span>
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your cart</span>
+      <span className={classes.badge}>3</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
