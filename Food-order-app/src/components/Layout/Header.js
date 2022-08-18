dc97e5b1daec0059e2abeb81c54e8d83d33f0bc1
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-18 16:23:50 +0000

    updated header.js

--- Food-order-app/src/components/Layout/Header.js
+++ Food-order-app/src/components/Layout/Header.js
@@ -1,7 +1,15 @@
 import { Fragment } from 'react';
+import mealsImage from '../../assets/mealsImage.jpg';
 
 const Header = props => {
-    return <Fragment></Fragment>
+    return <Fragment>
+        <header>
+            <h1>ReactMeals</h1>
+            <div>
+                <img src={mealsImage} alt='A table full of delicious food!' />
+            </div>
+        </header>
+    </Fragment>
 };
 
 export default Header;
