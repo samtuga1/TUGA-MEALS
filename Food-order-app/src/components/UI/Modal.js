0d14de63f47feb96a086371477f7338444207698
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 09:16:24 +0000

    update

--- Food-order-app/src/components/UI/Modal.js
+++ Food-order-app/src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 import  ReactDOM  from 'react-dom';
 
 const Backdrop = props => {
-    return <div className={classes.backdrop} />
+    return <div className={classes.backdrop} onClick={props.onClose} />
 };
 
 const ModalOverlay = props => {
@@ -16,7 +16,7 @@ const portalElement = document.getElementById('overlays');
 
 const Modal = props => {
     return <Fragment>
-        {ReactDOM.createPortal(<Backdrop />, portalElement )}
+        {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement )}
         {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement )}
     </Fragment>
 };
