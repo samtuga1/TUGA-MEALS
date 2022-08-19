91ebea806e7e8a0937fce0c4ddf38d60f89f6d72
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 23:41:36 +0000

    update

--- Food-order-app/src/components/UI/Input.js
+++ Food-order-app/src/components/UI/Input.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Input = React.forwardRef((props, ref) => {
     return <div className={classes.input} >
         <label htmlFor={props.input.id} > {props.label} </label>
-        <input {...props.input} />
+        <input {...props.input} ref={ref} />
     </div>
 });
 
