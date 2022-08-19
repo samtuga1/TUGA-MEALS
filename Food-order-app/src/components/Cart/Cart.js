45820fd79c26ad2636d5192fde5410c49b525850
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 00:02:09 +0000

    update

--- Food-order-app/src/components/Cart/Cart.js
+++ Food-order-app/src/components/Cart/Cart.js
@@ -1,7 +1,21 @@
-import classes from './Cart.module.css';
+import classes from "./Cart.module.css";
 
-const Cart = props => {
-    
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", price: 12.99, amount: 2 }].map((cart) => (
+        <li>{cart.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Cart;
