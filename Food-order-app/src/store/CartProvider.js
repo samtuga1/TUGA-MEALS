1f20cf863356db95cb173674309943e473346ac4
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 21:52:59 +0000

    update

--- Food-order-app/src/store/CartProvider.js
+++ Food-order-app/src/store/CartProvider.js
@@ -7,6 +7,17 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+    if(action.type === 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items : updatedItems,
+            totalAmount : updatedTotalAmount,
+        };
+    }
+    if(action.type === 'REMOVE'){
+        
+    }
     return defaultCartState;
 };
 
