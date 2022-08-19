2ed7c9f79468a2c9d941a9bffe510a3c0578e660
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 10:08:55 +0000

    update

--- Food-order-app/src/store/CartProvider.js
+++ Food-order-app/src/store/CartProvider.js
@@ -1,17 +1,30 @@
 import CartContext from "./cart-context";
 import { useReducer } from "react";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+};
 
+const cartReducer = (state, action) => {
+    return defaultCartState;
+};
 
 const CartProvider = props => {
 
-    const addItemToCartHandler = (item) => {};
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeItemToCartHandler = (id) => {};
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemToCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE', id: id});
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemToCartHandler,
     }
