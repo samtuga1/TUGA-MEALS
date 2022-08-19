296b3bbb1ed1320566550166c661a86969d94b8b
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 08:51:25 +0000

    update

--- Food-order-app/src/components/UI/Modal.js
+++ Food-order-app/src/components/UI/Modal.js
@@ -1,6 +1,6 @@
 import classes from './Modal.module.css';
 import { Fragment } from 'react';
-import  ReactDOM  from 'react';
+import  ReactDOM  from 'react-dom';
 
 const Backdrop = props => {
     return <div className={classes.backdrop} />
