a033e7a2f8f140b7acb69c7b425754a64947030e
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 22:20:13 +0000

    update

--- Food-order-app/src/components/UI/Input.js
+++ Food-order-app/src/components/UI/Input.js
@@ -1,10 +1,11 @@
 import classes from './Input.module.css';
+import React from 'react';
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
     return <div className={classes.input} >
         <label htmlFor={props.input.id} > {props.label} </label>
         <input {...props.input} />
     </div>
-};
+});
 
 export default Input;
