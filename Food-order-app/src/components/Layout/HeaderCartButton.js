70d5868bcf779e3e7cd2f2aed8da7a618c40fc78
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 09:54:59 +0000

    update

--- Food-order-app/src/components/Layout/HeaderCartButton.js
+++ Food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+
+const cartCtx = useContext(CartContext);
+
+const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  return curNumber + item.amount;
+}, 0);
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
