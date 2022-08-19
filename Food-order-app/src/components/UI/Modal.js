3f5fc33e382901be01e34381b87d47647aad4194
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 00:28:38 +0000

    update

---
+++ Food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,12 @@
+import classes from './Modal.module.css';
+
+const Backdrop = props => {
+    return 
+};
+
+const ModalOverlay = props => {};
+
+const Modal = props => {};
+
+
+export default Modal;
