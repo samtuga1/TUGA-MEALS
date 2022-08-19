6ccfc72c051c038ba668b55060ac3803f95be222
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 09:02:34 +0000

    update

--- Food-order-app/src/components/Cart/Cart.js
+++ Food-order-app/src/components/Cart/Cart.js
@@ -18,8 +18,8 @@ const Cart = (props) => {
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button onClick={props.onHideCart} className={classes["button--alt"]}>Close</button>
+        <button  className={classes.button}>Order</button>
       </div>
     </Modal>
   );
