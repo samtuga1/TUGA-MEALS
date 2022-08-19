00f07f6b67a2ae4ade085eb8a19144b390ed25fd
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-19 09:02:45 +0000

    update

--- Food-order-app/src/components/Layout/HeaderCartButton.js
+++ Food-order-app/src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
