1852c21021684061c4d3901ed52770bc83d840de
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-20 00:22:02 +0000

    update

--- Food-order-app/src/components/Cart/Cart.js
+++ Food-order-app/src/components/Cart/Cart.js
@@ -10,7 +10,9 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item, amount : 1});
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
