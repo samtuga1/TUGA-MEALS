eb42be100767e223c2e7c4d5e5548042c172bdb6
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-08 08:30:25 +0000

    update

---
+++ Food-order-app/src/components/Cart/Checkout.js
@@ -0,0 +1,100 @@
+import classes from "./Checkout.module.css";
+import { useRef, useState } from "react";
+
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
+const Checkout = (props) => {
+  const [formInputsValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    postal: true,
+    city: true,
+  });
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+    const name = nameInputRef.current.value;
+    const street = streetInputRef.current.value;
+    const postal = postalInputRef.current.value;
+    const city = cityInputRef.current.value;
+
+    const nameIsValid = !isEmpty(name);
+    const streetIsValid = !isEmpty(street);
+    const postalIsValid = isFiveChars(postal);
+    const cityIsValid = !isEmpty(city);
+
+    setFormInputValidity({
+      name: nameIsValid,
+      street: streetIsValid,
+      postal: postalIsValid,
+      city: cityIsValid,
+    });
+
+    const formIsValid =
+      nameIsValid && streetIsValid && postalIsValid && cityIsValid;
+
+    if (!formIsValid) {
+      return;
+    } 
+    props.onConfirm({
+        name,
+        street,
+        postal,
+        city,
+    });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.name ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.street ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+        {!formInputsValidity.street && <p>Please enter a valid street</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.postal ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalInputRef} />
+        {!formInputsValidity.postal && <p>Postal code must be 5 characters long</p>}
+      </div>
+      <div
+        className={`${classes.control} ${
+          formInputsValidity.city ? "" : classes.invalid
+        }`}
+      >
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+        {!formInputsValidity.city && <p>Please enter a valid city</p>}
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
